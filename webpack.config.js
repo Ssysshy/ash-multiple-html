14f88fad4546a06cc46db93c137480996fc05dfb
ssysshy <foreverzrd@icloud.com>
2018-03-30 09:58:36 +0800

    add: webpack-dev-server hot update

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     jquery: 'window.$'
   },
   loaders: {
-    less: true,
+    less: true,   // 默认开启，如不需要泽关闭，减少编译时间
     nunjucks: {
       options: {
         searchPaths: ['./view']
@@ -21,9 +21,18 @@ module.exports = {
     }
   },
   plugins: {},
-  proxy: {
-    host:  'http://localhost:8888',
-    match: /\/debug/
+  options: {
+    devServer: {
+      hot: true,
+      inline: true,
+      progress: true,
+      proxy: {
+        host:  'http://localhost:8888',
+        match: /\/debug/,
+        changeOrigin: false, //  默认false，如需要可开启
+        secure: false
+      },
+    }
   },
   publicPath: '',
   done() {
